818b85a3a63ab7d41e280547be7b38b112bace6b
ptnamd19292e <ptnamd19292@cusc.ctu.edu.vn>
2020-01-05 09:53:41 +0700

    Trang chủ

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TRANG CHỦ</title>
+</head>
+<body>
+    <?php
+    echo '<span style="color:blue;"><h3> Chào Mừng đến với trang Salomon  </h3></span>';
+    ?>
+</body>
+</html>
